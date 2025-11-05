7365de2eb9230af6d14d5b2df4294d661b6a87b8
Ashi-Ashirbaditya <129829025+Ashi-Ashirbaditya@users.noreply.github.com>
2025-11-05 20:19:01 +0530

    Add files via upload

---
+++ PHP/update.php
@@ -0,0 +1,70 @@
+<?php
+include "config.php";
+if (isset($_POST['update']))
+{
+	$firstname=$_POST['firstname'];
+	$user_id= $_POST['user_id'];
+	$lastname=$_POST['lastname'];
+	$email=$_POST['email'];
+	$gender=$_POST['gender'];
+	$password=$_POST['password'];
+	$sql="UPDATE `users` SET `firstname` ='$firstname', `lastname` = '$lastname', `email`='$email',`password`='$password',`gender`='$gender' WHERE `id` ='$user_id'";
+	$result=$conn->query($sql);
+	if ($result==TRUE)
+	{
+		echo "Record updated ";
+	}
+	else
+	{
+		echo "Error".$sql."<br>".$conn->error;
+	}
+}
+if (isset($_GET['id']))
+{
+$user_id=$_GET['id'];
+$sql="SELECT * FROM `users` WHERE `id` ='$user_id'";
+$result=$conn->query($sql);
+if ($result->num_rows>0)
+{
+$row=$result->fetch_assoc();
+
+	 $first_name=$row['firstname'];
+	$lastname=$row['lastname'];
+	$email=$row['email'];
+	$password=$row['password'];
+	$gender=$row['gender'];
+	$id=$row['id'];
+
+}
+?>
+<html>
+<body>
+<h2> User update form</h2>
+<form action="" method="POST">
+<fieldset>
+<legend> Personal Information:</legend>
+Firstname: <br>
+<input type="text" name="firstname" value="<?php echo $first_name;?>">
+<input type="hidden" name="user_id" value="<?php echo $id;?>"><br>
+Lastname: <br>
+<input type="text" name="lastname" value="<?php echo $lastname;?>"><br>
+Email: <br>
+<input type="email" name="email" value="<?php echo $email;?>"><br>
+Password:<br>
+<input type="password" name="password" value="<?php echo $password;?>"><br>
+Gender: <br>
+<input type="radio" name="gender" value="male" <?php if ($gender=='Male') {echo "checked";}?>>Male
+<input type="radio" name="gender" value="female" <?php if ($gender=='Female') {echo "checked";}?>>Female
+<br><br>
+<input type="submit" value="update" name="update">
+</fieldset>
+</form>
+</body>
+</html>
+<?php
+}
+else{
+	header ('Location: view.php');
+}
+
+?>
